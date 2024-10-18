b6b3ea1d6aeb17f76c6564f1a0732ba1a5b046d9
Gladson Routh <grauth65@gmail.com>
2024-10-18 19:27:57 +0530

    update sign in

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 
 function SignIn() {
@@ -8,6 +10,18 @@ function SignIn() {
     const [ email, setEmail ] = useState("");
     const [ password, setPassword ] = useState("");
 
+    const [ loginEmail, setLoginEmail ] = useState("");
+    const [ loginPassword, setLoginPassword ] = useState("");
+
+    const [ error, setError ] = useState("");
+    const [ userError, setUserError ] = useState(false);
+    const [ passwordError, setPasswordError ] = useState(false);
+    const [ emailError, setEmailError ] = useState(false);
+   
+   
+
+    // for page redirect
+    const navigate = useNavigate()
 
     function toggleForm() {
         setIsSign(!isSign);
@@ -28,8 +42,29 @@ function SignIn() {
         const result = response.then((data) => {
             return data.json()
         })
-        result.then((data) => console.log(data));
+        result.then((data) => {
+
+            if (data.error) {
+                setError(data.message);
+                data.error == 'username' ? `${setUserError(!userError)} ${setEmailError(false)} ${setPasswordError(false)}`:
+                data.error == "email" ? `${setEmailError(!emailError)} ${setUserError(false)} ${setPasswordError(false)}`:
+                data.error == "password" ? `${setPasswordError(!passwordError)} ${setEmailError(false)} ${setUserError(false)}`:
+                    `${setPasswordError(false)} ${setEmailError(false)} ${setUserError(false)}`
+            }
+
+            // pass to login page
+            setTimeout(() => {
+                if (data.key == 'success') {
+                    setEmailError(false);
+                    setPassword(false)
+                    setIsSign(!isSign)
+                }
+            }, 2000)
+
+            console.log(data)
+        });
         // console.log(username, email, password);
+        
     }
 
 
@@ -40,9 +75,9 @@ function SignIn() {
         const response = fetch("http://localhost:7100/login", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify({email: email, password: password})
+            body: JSON.stringify({email: loginEmail, password: loginPassword})
         })
 
         const result = response.then((data) => {
@@ -51,6 +86,18 @@ function SignIn() {
 
         result.then((data) => {
             localStorage.setItem("accessToken", data.accessToken)
+            localStorage.setItem("username", data.User)
+            if (data.error) {
+                setError(data.message);
+                data.error == 'email' ? `${setEmailError(true)} ${setPasswordError(false)}` :
+                data.error == 'password' ? `${setPasswordError(true)} ${setEmailError(false)}`:
+                data.error == "notRegistered" ? `${setPasswordError(false)} ${setEmailError(false)}` : ""
+            }
+
+            if (data.User) {
+                localStorage.setItem("login", 'true');
+                navigate("/home");
+            }
             console.log(data)
         })
     }
@@ -61,26 +108,30 @@ function SignIn() {
             {
                 isSign &&
 
-                <div className="bg-white w-7/12 lg:w-2/5 h-3/5 rounded-2xl drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]">
+                <>
+                <h1 className="absolute top-20 text-red-500 w-screen py-2 px-20 text-center">{error && error}</h1>
+                <div className="bg-white pb-7 w-7/12 lg:w-2/5 h-9/12 lg:h-3/5 rounded-2xl drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] overflow-hidden">
                     <h1 className="text-center mt-4 text-2xl">Sign in</h1>
-                    <form className="h-80 mt-7 flex flex-col justify-center gap-2 px-7 pb-10">
+                    <form className="h-80 mt-7 flex flex-col justify-center gap-2 px-7 pb-4">
                         
                         <label htmlFor="username">Username</label>
                         <input onChange={(e)=> setUsername(e.target.value)} id="username" type="text" required placeholder="Username"
-                            className="h-10 rounded-lg outline-none border-2 px-3"/>
+                            className={`h-10 rounded-lg outline-none border-2 px-3 ${userError && "outline-red-500"}`}/>
 
                         <label htmlFor="email">Email</label>
                         <input onChange={(e)=> setEmail(e.target.value)} id="email" type="email" required placeholder="Email"
-                            className="h-10 rounded-lg outline-none border-2 px-3"/>
+                            className={`h-10 rounded-lg outline-none border-2 px-3 ${emailError && "outline-red-500"}`}/>
 
                         <label htmlFor="password">Password</label>
                         <input onChange={(e)=> setPassword(e.target.value)} id="password" type="password" placeholder="Password"
-                            className="h-10 rounded-lg outline-none border-2 px-3"/>
-                    
-                        <button onClick={signIn} className="bg-slate-200 mx-32 py-2 rounded-xl 
-                            hover:bg-sky-200">
-                            Sign In
-                        </button>
+                            className={`h-10 rounded-lg outline-none border-2 px-3 ${passwordError && "outline-red-500"}`}/>
+                        
+                        <div className="flex justify-center items-center py-2">
+                            <button onClick={signIn} className="bg-slate-200 py-2 px-4 rounded-xl 
+                                hover:bg-sky-200">
+                                Sign In
+                            </button>
+                        </div>
                     </form>
 
                     <div className="flex justify-center mt-6">
@@ -91,27 +142,33 @@ function SignIn() {
                         </h2>
                     </div>
                 </div>
+                </>
             }
 
             {
                 !isSign &&
-
+                
+                <>
+                <h1 className="absolute top-20 text-red-500 w-screen py-2 px-20 text-center">{error && error}</h1>
                 <div className="bg-white w-7/12 lg:w-2/5 h-3/5 rounded-2xl drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]">
                     <h1 className="text-center mt-4 text-2xl">Log in</h1>
                     <form className="h-80 mt-7 flex flex-col justify-center gap-2 px-7 pb-10">
 
                         <label htmlFor="email">Email</label>
-                        <input id="email" type="email" required placeholder="Email"
-                            className="h-10 rounded-lg outline-none border-2 px-3"/>
+                        <input onChange={(e)=> setLoginEmail(e.target.value)} id="email" type="email" required placeholder="Email"
+                            className={`h-10 rounded-lg outline-none border-2 px-3 ${emailError && "outline-red-500"}`}/>
 
                         <label htmlFor="password">Password</label>
-                        <input id="password" type="password" placeholder="Password"
-                            className="h-10 rounded-lg outline-none border-2 px-3"/>
-                    
-                        <button onClick={logIn} className="bg-slate-200 mx-32 py-2 rounded-xl 
-                            hover:bg-sky-200">
-                            Log In
-                        </button>
+                        <input onChange={(e)=> setLoginPassword(e.target.value)} id="password" type="password" placeholder="Password"
+                            className={`h-10 rounded-lg outline-none border-2 px-3 ${passwordError && "outline-red-500"}`}/>
+
+                        <div className="flex justify-center items-center">
+                            <button onClick={logIn} className="bg-slate-200  mt-7 px-6 py-2 rounded-xl 
+                                    hover:bg-sky-200">
+                                    Log In
+                            </button>
+                        </div>
+
                     </form>
 
                     <div className="flex justify-center mt-6">
@@ -122,6 +179,7 @@ function SignIn() {
                         </h2>
                     </div>
                 </div>
+                </>
             }
             
             
